c8b16fd6a143b672ea2eccbfc8f60b3baf1fe91d
ktama <ktama@users.noreply.github.com>
2017-10-01 23:10:56 +0900

    関数名の変更およびlxmlの動作確認

--- Test.py
+++ Test.py
@@ -1,10 +1,19 @@
 import requests
-import lxml
+# import beautifulsoup4 2系でのみ動作？
+import lxml.html
 
 
-def Func():
+def CallRequests():
     r = requests.get('https://www.google.co.jp/')
     print(r.status_code)
 
+'''
+実行時にエラーが出る
+def CallLxml():
+    root = lxml.html.parse('https://www.google.co.jp/').getroot()
+    print(root.cssselect('title')[0].text)
+'''
+
 if __name__ == '__main__':
-    Func()
+    CallRequests()
+    CallLxml()
